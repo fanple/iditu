08f0c21d3396701d5c968370a44ca0f1dc2b1325
zhang jun jie <zhangjunjie@live.com>
2014-03-24 17:29:08 +0800

    Client: When click "Iamhere" ICON start an animation.

--- Other/Client/gen/net/iditu/R.java
+++ Other/Client/gen/net/iditu/R.java
@@ -8,6 +8,9 @@
 package net.iditu;
 
 public final class R {
+    public static final class anim {
+        public static final int shortcut_onekey=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,8 +20,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int button_down=0x7f020000;
@@ -33,33 +36,41 @@ public final class R {
         public static final int popup_middle=0x7f020009;
         public static final int popup_right=0x7f02000a;
         public static final int popup_side=0x7f02000b;
+        public static final int shortcut_onekey_bg=0x7f02000c;
+        public static final int shortcut_onekey_cover=0x7f02000d;
+        public static final int shortcut_onekey_rotate=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int bmapView=0x7f080000;
-        public static final int btLocation=0x7f080004;
-        public static final int customicon=0x7f080003;
-        public static final int defaulticon=0x7f080002;
-        public static final int popdown=0x7f080008;
-        public static final int popinfo=0x7f080006;
-        public static final int popleft=0x7f080005;
-        public static final int popright=0x7f080009;
-        public static final int radioGroup=0x7f080001;
-        public static final int textcache=0x7f080007;
+        public static final int action_settings=0x7f09000e;
+        public static final int anim_image=0x7f09000c;
+        public static final int anim_layout=0x7f09000b;
+        public static final int bmapView=0x7f090000;
+        public static final int btLocation=0x7f090004;
+        public static final int cover_image=0x7f09000d;
+        public static final int customicon=0x7f090003;
+        public static final int defaulticon=0x7f090002;
+        public static final int main=0x7f09000a;
+        public static final int popdown=0x7f090008;
+        public static final int popinfo=0x7f090006;
+        public static final int popleft=0x7f090005;
+        public static final int popright=0x7f090009;
+        public static final int radioGroup=0x7f090001;
+        public static final int textcache=0x7f090007;
     }
     public static final class layout {
         public static final int iditumainmap=0x7f030000;
         public static final int popupbubles=0x7f030001;
+        public static final int shortcut_onekey=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int demo_name_location=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int iamhere=0x7f050004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int demo_name_location=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int iamhere=0x7f060004;
     }
     public static final class style {
         /** 
@@ -83,10 +94,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Other/Client/src/net/iditu/IamHereActivity.java
+++ Other/Client/src/net/iditu/IamHereActivity.java
@@ -1,14 +1,61 @@
 package net.iditu;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class IamHereActivity extends Activity {
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.iditumainmap);
-	}
+	FrameLayout bg=null;
+    @Override
+    public void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+  
+        Intent intent = getIntent();
+        setContentView(R.layout.shortcut_onekey);
+        bg = (FrameLayout)findViewById(R.id.anim_layout);
+        Rect rect = intent.getSourceBounds();
+        if(rect != null)
+        {
+			//int i = getResources().getDimensionPixelSize(0x7f0b001d);
+            int i=10;
+        	int j = rect.left + rect.width() / 2;
+            int k = i + rect.top;
+            android.widget.RelativeLayout.LayoutParams layoutparams = (android.widget.RelativeLayout.LayoutParams)bg.getLayoutParams();
+            layoutparams.leftMargin = j - layoutparams.width / 2;
+            layoutparams.topMargin = k - layoutparams.height / 2;
+            bg.setLayoutParams(layoutparams);
+        }
+        play(this);
+        mHandler.sendEmptyMessageDelayed(1, 1000);
+    }
+    public static void play(IamHereActivity onekeyactivity)
+    {
+        onekeyactivity.bg.setVisibility(0);
+        ImageView imageview = (ImageView)onekeyactivity.findViewById(R.id.anim_image);
+        imageview.setVisibility(0);
+        imageview.startAnimation(AnimationUtils.loadAnimation(onekeyactivity, R.anim.shortcut_onekey));
+    }
+
+    
+	private Handler mHandler = new Handler() {
+
+		public void handleMessage(Message msg) {
+
+			Toast.makeText(getApplicationContext(), "toast handle message",
+				     Toast.LENGTH_SHORT).show();
+
+			if (1 == msg.what)
+				finish(); // 动画播放结束，结束我们的Activity界面
+		}
+	};
 
 }
